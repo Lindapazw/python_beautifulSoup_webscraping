910349fbd430934faa9cc1625fb62c0379e45cfc
Linda Paz <lindacsndelapaz@gmail.com>
2023-03-21 12:14:45 -0300

    add python-docx

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+from docx import Document
 
 # website
 website = 'https://subslikescript.com/movie/Titanic-120338'
+
 # pongo en la variable result el resultado
 result = requests.get(website)
+
 # creo la variable content y coloco el result y obtengo el texto
 content = result.text
+
 # creamos la variable soup para localizar elementos dentro de la web
 soup = BeautifulSoup(content, 'lxml')
 
@@ -19,7 +23,18 @@ title = box.find('h1').get_text()
 # creamos la variable transcript
 transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
 
-#imprimimos
+# imprimimos
 print(title)
 print(transcript)
 
+# Creamos un archivo de Word
+document = Document()
+
+# Agregamos el título al archivo de Word
+document.add_heading(title, 0)
+
+# Agregamos el texto al archivo de Word
+document.add_paragraph(transcript)
+
+# Guardamos el archivo
+document.save('transcript.docx')
