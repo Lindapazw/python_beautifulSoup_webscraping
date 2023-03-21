24edcad74eee77e5bde2f729387a636c4b6b1a80
Linda Paz <lindacsndelapaz@gmail.com>
2023-03-21 11:31:19 -0300

    add soup, content, result and print lxml

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+# website
 website = 'https://subslikescript.com/movie/Titanic-120338'
-requests.get(website)
+# pongo en la variable result el resultado
+result = requests.get(website)
+# creo la variable content y coloco el result y obtengo el texto
+content = result.text
+# creamos la variable soup para localizar elementos dentro de la web
+soup = BeautifulSoup(content, 'lxml')
+# imprimimos el html
+print(soup.prettify())
 
