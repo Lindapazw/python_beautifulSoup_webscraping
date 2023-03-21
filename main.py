4087558dc22504ce454b224334a05f7228a05fb1
Linda Paz <lindacsndelapaz@gmail.com>
2023-03-21 11:54:28 -0300

    add box and print title

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ result = requests.get(website)
 content = result.text
 # creamos la variable soup para localizar elementos dentro de la web
 soup = BeautifulSoup(content, 'lxml')
-# imprimimos el html
-print(soup.prettify())
 
+# creamos un box con el contenido del article
+box = soup.find('article', class_='main-article')
+
+# en lugar de soup.find ponemos box para referirnos al article
+title = box.find('h1').get_text()
+print(title)
